53abb1f1554ac040f2ece6ed205e2434d5967bc2
Victor Wagner <victorwdr@gmail.com>
2017-07-13 17:15:18 -0300

    a

--- Model/User.cs
+++ Model/User.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Model
 {
-    public class User
+    public class User : IIdentify
     {
         public int Id { get; set; }
         public string Name { get; set; }
--- Persistence/File.cs
+++ Persistence/File.cs
@@ -2,20 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 using System.IO;
-using System.Web.Script.Serialization;
 
 namespace Persistence
 {
-    static class File<T>
+    static class File<T> where T : Model.IIdentify
     {
         public static List<T> Open(string file)
         {
             List<T> content = null;
             try
             {
-                JavaScriptSerializer js = new JavaScriptSerializer();
-                content = js.Deserialize<List<T>>(file);
+                using (StreamReader sr = new StreamReader(file, Encoding.Default))
+                using (JsonReader reader = new JsonTextReader(sr))
+                {
+                    JsonSerializer js = new JsonSerializer();
+                    content = js.Deserialize<List<T>>(reader);
+                }
             }
             catch
             {
@@ -26,8 +31,41 @@ namespace Persistence
 
         public static void Save(string file, List<T> objs)
         {
-            JavaScriptSerializer js = new JavaScriptSerializer();
-            file = js.Serialize(objs);
+            using (StreamWriter sw = new StreamWriter(file, false, Encoding.Default))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                JsonSerializer js = new JsonSerializer();
+                js.Serialize(writer, objs);
+            }
+        }
+
+        public static List<T> Select(string arquivo)
+        {
+            return Open(arquivo);
+        }
+
+        public static void Insert(string file, T obj)
+        {
+            List<T> objs = Select(file);
+            objs.Add(obj);
+            Save(file, objs);
+        }
+
+        public static void Update(string file, T obj)
+        {
+            List<T> objs = Select(file);
+            T x = objs.Where(r => r.Id == obj.Id).Single();
+            objs.Remove(x);
+            objs.Add(obj);
+            Save(file, objs);
+        }
+
+        public static void Delete(string file, T obj)
+        {
+            List<T> objs = Select(file);
+            T x = objs.Where(r => r.Id == obj.Id).Single();
+            objs.Remove(x);
+            Save(file, objs);
         }
     }
 }
