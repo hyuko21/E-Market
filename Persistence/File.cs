e36989b7b45a0a4af3b11d12706e82c04bb91edf
Victor Wagner <victorwdr@gmail.com>
2017-07-13 15:15:09 -0300

    a

--- Persistence/File.cs
+++ Persistence/File.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using System.Web.Script.Serialization;
 
 namespace Persistence
 {
-    class File
+    static class File<T>
     {
+        public static List<T> Open(string file)
+        {
+            List<T> content = null;
+            try
+            {
+                JavaScriptSerializer js = new JavaScriptSerializer();
+                content = js.Deserialize<List<T>>(file);
+            }
+            catch
+            {
+                content = new List<T>();
+            }
+            return content;
+        }
+
+        public static void Save(string file, List<T> objs)
+        {
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            file = js.Serialize(objs);
+        }
     }
 }
