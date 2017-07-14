970ebefe3d8ace11b93efc03b2f07e359c0f0f5e
Viktor Kovarowski <victorwdr@outlook.com>
2017-07-13 21:54:18 -0300

    A

--- Business/AccessLog.cs
+++ Business/AccessLog.cs
@@ -7,5 +7,26 @@ namespace Business
 {
     public class AccessLog
     {
+        private Persistence.AccessLog pLog = new Persistence.AccessLog();
+
+        public List<Model.AccessLog> Select()
+        {
+            return pLog.Select();
+        }
+
+        public void Insert(Model.AccessLog log)
+        {
+            pLog.Insert(log);
+        }
+
+        public void Update(Model.AccessLog log)
+        {
+            pLog.Update(log);
+        }
+
+        public void Delete(Model.AccessLog log)
+        {
+            pLog.Delete(log);
+        }
     }
 }
--- Business/User.cs
+++ Business/User.cs
@@ -7,6 +7,30 @@ namespace Business
 {
     public class User
     {
+        private Persistence.User pUser = new Persistence.User();
 
+        public List<Model.User> Select()
+        {
+            return pUser.Select();
+        }
+
+        public void Insert(Model.User u)
+        {
+            if (u == null)
+                throw new ArgumentNullException();
+            if (pUser.Select().Where(r => r.Name == u.Name).Count() > 0)
+                throw new InvalidOperationException();
+            pUser.Insert(u);
+        }
+
+        public void Update(Model.User u)
+        {
+            pUser.Update(u);
+        }
+
+        public void Delete(Model.User u)
+        {
+            pUser.Delete(u);
+        }
     }
 }
